b0df90288c166b12493cc0259a13eb9409f71e98
Clement Liscoet <clement.liscoet@gmail.com>
2021-03-29 20:40:30 +0100

    feat: functions to prepare dataFrame

--- wt_functions.py
+++ wt_functions.py
@@ -2,6 +2,69 @@ import pandas as pd
 import numpy as np
 
 
+def get_file(path_name):
+    """return a dataFrame from cvs file
+
+    Args:
+        path_name ([str]): path to the file
+
+    Returns:
+        [dataFrame]
+    """
+    return pd.read_csv(path_name)
+
+
+def set_index_col(df, col):
+    """set a given column to index
+
+    Args:
+        df ([dataFrame]):
+        col ([obj]): 
+
+    Returns:
+        [dataFrame]:
+    """
+    df = df.set_index(df[col])
+    return df
+
+
+def trim_df(df, *cols):
+    """Removed unwanted column to a DataFrame
+    Args:
+        df ([DataFrame]): a dataFrame
+    Returns:
+        [list]: list of columns name 
+    """
+    len_col_origin = len(df.columns)
+    for col in cols:
+        del df[col]
+    len_col_trimed = len(df.columns)
+
+    if len_col_origin > len_col_trimed:
+        return '{del_col} column(s) deleted'.format(del_col=len_col_origin - len_col_trimed)
+
+
+def prep_index(path_name, col):
+
+    df = get_file(path_name)
+    df = set_index_col(df, col)
+    return df
+
+
+def rename_col(df, origin_col_name, new_col_name):
+    """Rename a column name
+
+    Args:
+        df ([datFrame]): the dataFrame that included the column
+        origin_col_name ([str]): the name of the original column
+        new_col_name ([str]):the new name
+
+    Returns:
+        [dataFrame]: the dataFrame with the newly named column
+    """
+    return df.rename({origin_col_name: new_col_name}, axis='columns')
+
+
 def get_temperature(i, col):
     return round(df.loc[i:i+9, col].mean(axis=0), 2)
 
@@ -19,6 +82,18 @@ def insert_data(data):
 
 def main():
     print('active functions module')
+    # df = get_file('./data/wt_data.csv')
+
+    df = prep_index('./data/wt_data.csv', 'year')
+
+    city_name = df.iloc[0][0]
+    col_name_updated = f'temp_city {city_name }'
+
+    df = rename_col(df, 'temp_ciy', col_name_updated)
+
+    trim_df(df, 'year', 'city')
+
+    print(df)
 
 
 if(__name__ == '__main__'):
