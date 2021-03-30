a961b17297b9a3b0f6f9e75f8d9c6115f541adec
Clement Liscoet <clement.liscoet@gmail.com>
2021-03-30 02:17:16 +0100

    documentation: add Docstring to wt_function module

--- wt_functions.py
+++ wt_functions.py
@@ -59,16 +59,51 @@ def rename_col(df, origin_col_name, new_col_name):
 
 
 def get_temperature(df, i, col):
+    """group year by range of 9
+    and return average temperature for a given column
+
+    Args:
+        df ([dataFrame]): 
+        i ([int]): the starting year of the decade
+        col ([str]): name of the column
+
+    Returns:
+        [float]: average of temperature for year range
+    """
     return round(df.loc[i:i+9, col].mean(axis=0), 2)
 
 
 def get_row_val(df, i, year_max, col_city, col_world):
+    """return value for average temperature for 
+    range of year. 
+
+    Args:
+        df ([datFrame]): 
+        i ([int]): teh first year of the decade
+        year_max ([int]): the max year use a test to stop
+        col_city ([str]): name of column city
+        col_world ([str]): name of column world_temp
+
+    Returns:
+        [list]: return list of essentail value for each row
+        year (index), year range, temperature for city picked, temperature for world
+    """
+    # text if i_plus if bigger means that needs to take value of
+    # year_max instead. Since behond of last range.
     i_plus = year_max if year_max < i+9 else i+9
     year_range = f'{i}-{i_plus}'
     return [i, year_range, get_temperature(df, i, col_city), get_temperature(df, i, col_world)]
 
 
 def insert_data(data, index_list, data_list):
+    """insert values to index_list and data_list
+       in order to be added to new dataFrae
+
+    Args:
+        data ([list]): list of value
+        index_list ([list]):
+        data_list ([list]): 
+    """
     index_list.append(data[0])
     data_list.append(data[1:5])
 
